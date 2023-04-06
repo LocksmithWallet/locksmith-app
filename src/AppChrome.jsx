15bece07610b448fdb3c43149eca87092c2dd338
scotthconner <scotthconner@gmail.com>
2023-04-05 19:46:01 -0500

    another test

--- src/AppChrome.jsx
+++ src/AppChrome.jsx
@@ -163,6 +163,7 @@ const MobileNav = ({ onOpen, ...rest }) => {
               <motion.div 
                 initial={{opacity: 0, y: -100}}
                 animate={{opacity: 1, y: 0}}
+                exit={{opacity: 0, y: -100}}
                 transition={{ type: 'spring'}}>
                   <NetworkSwitcher/> 
               </motion.div>)}
