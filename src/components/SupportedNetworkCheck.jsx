ac284f55456273b52ce959643ab778b563878a9e
scotthconner <scotthconner@gmail.com>
2023-04-19 19:22:25 -0500

    Fixed a bug where you weren't logged in and got max depth navigate

--- src/components/SupportedNetworkCheck.jsx
+++ src/components/SupportedNetworkCheck.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useNetwork } from 'wagmi';
 import { Networks } from '../configuration/Networks';
 
 export const SupportedNetworkCheck = ({children}) => {
   const network = useNetwork();
   const navigate = useNavigate();
-
-  if(!(network && network.chain && network.chain.id && 
+  const location = useLocation();
+ 
+  if(location.pathname !== '/' && !(network && network.chain && network.chain.id && 
       Networks.getNetwork(network.chain.id) !== undefined)) {
         navigate('/');
         return ''
