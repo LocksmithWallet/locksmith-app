af9130ac48bac97c1790a4a852ce03355829c7ad
scotthconner <scotthconner@gmail.com>
2023-05-09 21:33:11 -0500

    Fixed trust creation with the new interface and verified that distribution works

--- src/hooks/contracts/TrustCreator.jsx
+++ src/hooks/contracts/TrustCreator.jsx
@@ -4,8 +4,10 @@ import {
 }from '../Utils';
 import {
   usePrepareContractWrite,
-  useContractWrite
+  useContractWrite,
+  useNetwork,
 } from 'wagmi';
+import { Networks } from '../../configuration/Networks';
 import {ethers} from 'ethers';
 
 
@@ -15,8 +17,14 @@ import {ethers} from 'ethers';
  * Mints a Trust and a root key, and sends it to the caller. 
  */
 export function useMintTrust(trustName, errorFunc, successFunc) {
+  const network = useNetwork();
   return useLocksmithWrite('TrustCreator', 'spawnTrust',
-      [ethers.utils.formatBytes32String(trustName.trim()), [], [], []],
+      [ethers.utils.formatBytes32String(trustName.trim()), [], [], [],
+        [Networks.getContractAddress(network.chain.id, 'Distributor')],
+        [ethers.utils.formatBytes32String('Distributor Scribe')],
+        [],
+        []
+      ],
       true, 
       errorFunc, successFunc);
 }
