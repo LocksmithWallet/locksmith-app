fd65cd3b921d409b9010a349d19402b4c500721d
scotthconner <scotthconner@gmail.com>
2023-05-12 15:33:34 -0500

    Added Drawer for transaction history Entries.

--- src/navigation/TransactionHistory.jsx
+++ src/navigation/TransactionHistory.jsx
@@ -1,27 +1,70 @@
 import { 
   Box,
   Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  List,
+  ListItem,
   Text,
+  useDisclosure,
 } from '@chakra-ui/react';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { AiOutlineBell } from 'react-icons/ai';
 import { TransactionListContext } from '../components/TransactionProvider';
 
 export const TransactionHistoryButton = () => {
   const transactions = useContext(TransactionListContext);
+  const disclosure = useDisclosure();
 
-  return transactions.transactions.length > 0 && (<Button size='sm' boxShadow='lg' pos='relative'>
-    <Box 
-      pos='absolute'
-      bg='red.500'
-      top='-0.5em'
-      right='-0.5em'
-      fontSize='0.9em'
-      borderRadius='full' p='0.3em' pl='0.5em' pr='0.5em'>
-      <Text textColor='white' fontWeight='bold'>{transactions.notificationCount}</Text>
-    </Box>
-    <Box pos='absolute'>
-      <AiOutlineBell size='20px'/>
-    </Box>
-  </Button>)
+  return transactions.transactions.length > 0 && (
+    <Button size='sm' boxShadow='lg' pos='relative' onClick={disclosure.onOpen}>
+      <Box 
+        pos='absolute'
+        bg='red.500'
+        top='-0.5em'
+        right='-0.5em'
+        fontSize='0.9em'
+        borderRadius='full' p='0.3em' pl='0.5em' pr='0.5em'>
+        <Text textColor='white' fontWeight='bold'>{transactions.notificationCount}</Text>
+      </Box>
+      <Box pos='absolute'>
+        <AiOutlineBell size='20px'/>
+      </Box>
+      <TransactionHistoryDrawer disclosure={disclosure}/>
+    </Button>)
 };
+
+export const TransactionHistoryDrawer = ({disclosure}) => {
+  const transactions = useContext(TransactionListContext);
+  const btnRef = useRef();
+
+  return (<Drawer
+    isOpen={disclosure.isOpen}
+    placement='right'
+    onClose={disclosure.onClose}
+    finalFocusRef={btnRef}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton size='lg'/>
+        <DrawerHeader>Transaction History</DrawerHeader>
+        <DrawerBody>
+          <List>
+            {transactions.transactions.map((txn) => (
+              <TransactionHistoryEntry txn={txn}/>
+            ))} 
+          </List>
+        </DrawerBody>
+        <DrawerFooter>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>) 
+}
+
+export const TransactionHistoryEntry = ({txn, ...rest}) => {
+  return <Text>{txn.hash}</Text>
+}
