ac316016e89e69c2c16442bb18d185ef1b13f5e8
scotthconner <scotthconner@gmail.com>
2023-05-24 11:22:35 -0500

    Add some test assets to base goerli

--- src/configuration/networks/BaseGoerli.jsx
+++ src/configuration/networks/BaseGoerli.jsx
@@ -1,10 +1,16 @@
 import { baseGoerli } from "wagmi/chains";
 import { BaseIcon } from '../../components/icons/Base.jsx';
-import { GAS_ARN } from '../AssetResource';
+import {
+  GAS_ARN,
+  getAssetResourceName,
+} from '../AssetResource';
 
 import * as contracts from '../contracts/network-contracts-84531.json';
+import * as assets from '../assets/network-assets-84531.json';
 
 import { ETH } from '../../components/icons/ETH';
+import { LINK } from '../../components/icons/LINK';
+import { USDC } from '../../components/icons/USDC';
 
 export const BaseGoerli = (function() {
   return {
@@ -30,6 +36,32 @@ export const BaseGoerli = (function() {
           return <ETH {...props} color='#716b94'/>;
         }
       }
+
+      list[getAssetResourceName(assets['link'].address, 20, 0)] = {
+        name: 'Chainlink',
+        symbol: 'LINK',
+        decimals: 18,
+        contractAddress: assets['link'].address,
+        standard: 20,
+        id: 0,
+        coinCapId: 'chainlink',
+        icon: function(props = {}) {
+          return <LINK {...props} color='#375BD2'/>;
+        }
+      };
+
+      list[getAssetResourceName(assets['usdc'].address, 20, 0)] = {
+        name: 'USD Coin',
+        symbol: 'USDC',
+        decimals: 6,
+        contractAddress: assets['usdc'].address,
+        standard: 20,
+        id: 0,
+        coinCapId: 'usd-coin',
+        icon: function(props = {}) {
+          return <USDC color='#2775ca' {...props}/>;
+        }
+      };
 
       return list;
     })(),
