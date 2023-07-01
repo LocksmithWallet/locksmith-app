4afd7881c2c86dca8c6225e4a18131a55b859a4e
scotthconner <scotthconner@gmail.com>
2023-07-01 14:04:50 -0500

    Addded the wobble box so when you click on a key the detail pops up.

--- src/Trust.jsx
+++ src/Trust.jsx
@@ -1,7 +1,12 @@
 //////////////////////////////////////
 // React and UI Components
 //////////////////////////////////////
-import { React, useState, useEffect } from 'react';
+import {
+  React, 
+  useState,
+  useEffect,
+  useRef
+} from 'react';
 import {
   useParams
 } from 'react-router-dom';
@@ -20,6 +25,8 @@ import {
   Text,
   Spacer,
   VStack,
+  useBreakpointValue,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { ethers } from 'ethers';
 import {
@@ -43,7 +50,11 @@ import {
 import { ContextBalanceUSD } from './components/Ledger';
 import { KeyIcon } from './components/Key';
 
-import { motion, AnimatePresence } from 'framer-motion';
+import { 
+  motion,
+  AnimatePresence,
+  useAnimation,
+} from 'framer-motion';
 import { AiOutlineNumber } from 'react-icons/ai';
 
 export function Trust() {
@@ -98,45 +109,112 @@ const TrustHeader = ({trustId, trustInfo, ...rest}) => {
 }
 
 const TrustKeyList = ({trustId, trustInfo, trustKeys, ...rest}) => {
-  return (<List spacing='1.8em' m='1em' mt='2em'>
-    <AnimatePresence>
-      { trustKeys.map((k,x) => <ListItem key={'tkli-'+k.toString()} as={motion.div}
-        initial={{x: '100vh'}}
-        animate={{x: 0, transition: {delay: 0.125 + 0.05*x}}}
-        exit={{x: '100vh'}}>
+  const initialX = useBreakpointValue({base: '140vw', md: '100vw'});
+  return (<motion.div initial={{x: initialX}} animate={{x: 0}} transition={{delay: 0.25}}>
+    <List spacing='1.8em' m='1em' mt='2em'>
+      <AnimatePresence>
+        { trustKeys.map((k,x) => <ListItem key={'tkli-'+k.toString()} pos='relative'>
           <TrustKeyListItem keyId={k}/>
-      </ListItem>) }
-    </AnimatePresence>
-  </List>)
+        </ListItem>) }
+      </AnimatePresence>
+    </List>
+  </motion.div>)
 }
 
 const TrustKeyListItem = ({keyId, ...rest}) => {
   const keyInfo = useInspectKey(keyId);
+  const isDesktop = useBreakpointValue({base: false, md: true});
+  const animation = useAnimation();
+  const detailDisclosure = useDisclosure();
+  const ref = useRef(null);
 
-  return (<Box bg='white' borderRadius='lg' boxShadow='lg' p='0.8em' overflow='hidden' pos='relative'>
-    {keyInfo &&
-      <motion.div key={'kmo'+keyId} initial={{x: '100vw'}} animate={{x: 0}} transition={{duration: 0.2, delay: 0.3}}> 
+  const boxVariants = {
+    start: {
+    },
+    click: function() {
+      const rect = ref.current.getBoundingClientRect();
+      return {
+        position: 'fixed',
+        width: rect.width,
+        height: rect.height,
+        zIndex: 101,
+      };
+    },
+    open: function() {
+      const rect = ref.current.getBoundingClientRect();
+      return {
+        y: -1 * (rect.y) - window.scrollY,
+        zIndex: 500,
+        minWidth: isDesktop ? '0' : '92vw',
+        marginTop: '3vh',
+        height: '95vh',
+      }
+    },
+    close: {
+      x: 0,
+      y: 0,
+      zIndex: 0,
+      position: null,
+      height: null,
+      marginTop: '0vh',
+    },
+    final: {
+      width: null,
+    }
+  };
+
+  const toggleDetail = function() {
+    if (!detailDisclosure.isOpen) {
+      detailDisclosure.onOpen();
+      setTimeout(() => {
+        animation.set('click');
+        animation.start('open');
+      }, 1);
+    } else {
+      detailDisclosure.onClose();
+      setTimeout(async () => {
+        await animation.start('close');
+        animation.set('final');
+      }, 25);
+    }
+  };
+
+  const swipeProps = useBreakpointValue({base: {
+    drag: 'y',
+    onDragEnd: function(event, info) {
+      if (Math.abs(info.offset.y) >= 10 ) {
+        toggleDetail();
+      }
+    }
+  }, md: {}});
+
+  return (<motion.div key={'jiggle-box-'+keyId} animate={animation} variants={boxVariants}
+    {... (detailDisclosure.isOpen ? swipeProps : {})}>
+    <Box ref={ref} bg='white' borderRadius='lg' boxShadow='lg' p='0.8em' overflow='hidden' 
+      style={{height: '100%', zIndex: 0}} pos='relative'
+      onClick={() => { if(!detailDisclosure.isOpen) { toggleDetail(); } }}>
+      {keyInfo && <motion.div key={'kmo'+keyId} initial={{x: '100vw'}} animate={{x: 0}} transition={{duration: 0.2, delay: 0.3}}> 
         <KeyIcon keyInfo={keyInfo} size='80px' 
           style={{
-            filter: 'drop-shadow(0 2px 3px rgba(0, 0, 0, 0.5))',
             opacity: 0.5,
             position: 'absolute',
             left: '-16px',
             top: '-20px'
           }
         }/>
-        <VStack pos='absolute' width='3em' top='2px'>
+        <VStack pos='absolute' width='3em' top='4px'>
           <Text fontSize='xs'>#{keyId.toString()}</Text>
         </VStack>
       </motion.div> }
-    <HStack pl='4em'>
-      <Text fontWeight='bold'>{keyInfo && keyInfo.alias}</Text>
-      <Spacer/>
-      <ContextBalanceUSD contextId={KEY_CONTEXT} identifier={keyId}
-        skeletonProps={{}}
-        textProps={{
-          fontSize: 'md',
-        }}/>
-    </HStack>
-  </Box>)
+      <HStack pl='4em'>
+        <Text fontWeight='bold'>{keyInfo && keyInfo.alias}</Text>
+        <Spacer/>
+        <ContextBalanceUSD contextId={KEY_CONTEXT} identifier={keyId}
+          skeletonProps={{}}
+          textProps={{
+            fontSize: 'md',
+          }}/>
+      </HStack>
+    </Box>
+  </motion.div>)
 }
