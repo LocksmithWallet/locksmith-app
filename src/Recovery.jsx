e71b0969c4985917a02223e2dcb2914c45398498
scotthconner <scotthconner@gmail.com>
2023-08-02 17:00:24 -0500

    Nearly finished the onboarding flow

--- src/Recovery.jsx
+++ src/Recovery.jsx
@@ -28,6 +28,7 @@ import {
   Tag,
   TagLabel,
   TagLeftIcon,
+  Select,
   Spacer,
   Spinner,
   VStack,
@@ -83,6 +84,7 @@ import {
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { MdHealthAndSafety } from 'react-icons/md';
 import { FiEdit2, FiTrash2 } from 'react-icons/fi';
+import { FcAlarmClock } from 'react-icons/fc';
 import { AiOutlineNumber } from 'react-icons/ai';
 
 export const ConfigureRecoveryAlertIngress = ({keyInfo, ...rest}) => {
@@ -221,8 +223,7 @@ export function RecoveryCreateWizard({keyId, ...rest}) {
 
   // transaction parameters
   const [guardians, setGuardians] = useState([]);
-  const [deadmen, setDeadmen] = useState([]);
-  const [oracles, setOracles] = useState([]);
+  const [days, setDays] = useState(90);
 
   return (
     <VStack mt='4em' spacing='0'>
@@ -237,7 +238,15 @@ export function RecoveryCreateWizard({keyId, ...rest}) {
           </motion.div> }
           { step === 2 && <motion.div key='add-recovery-2' {... animations}>
             <StepTwoContent keyId={keyId} setStep={setStep}
-              guardians={guardians} setGuardians={setGuardians}/>
+              guardians={guardians} setGuardians={setGuardians}/> 
+          </motion.div> }
+          { step === 3 && <motion.div key='add-recovery-3' {... animations}>
+            <StepThreeContent keyId={keyId} setStep={setStep}
+              guardians={guardians} days={days} setDays={setDays}/>
+          </motion.div> }
+          { step === 4 && <motion.div key='add-recovery-4' {... animations}>
+            <StepFourContent keyId={keyId} setStep={setStep}
+              guardians={guardians} days={days}/>
           </motion.div> }
         </AnimatePresence>
       </Box>
@@ -252,7 +261,7 @@ export function StepZeroContent({keyId, setStep, ...rest}) {
       <HStack width='100%'>
         <Tag size='lg'><TagLabel>1</TagLabel></Tag>
         <VStack align='stretch' spacing='0em'>
-          <Text fontWeight='bold'>Choose Guardians</Text>
+          <Text fontWeight='bold'>Choose Recovery Addresses</Text>
           <Text fontSize='sm' color='gray'>Enable Master Key recovery.</Text>
         </VStack>
       </HStack>
@@ -273,28 +282,35 @@ export function StepOneContent({keyId, setStep, guardians, setGuardians, ...rest
   const [guardian, setGuardian] = useState(null); 
   const isValidAddress = ethers.utils.isAddress(guardian);
 
+  const canAddAddress = function() {
+    return isValidAddress && guardians.indexOf(guardian) < 0;
+  };
+
   return <VStack spacing='2em'>
     <HStack width='100%'>
       <Tag size='lg'><TagLabel>1</TagLabel></Tag>
-      <Text fontWeight='bold'>Choose Guardians</Text>
+      <Text fontWeight='bold'>Choose Recovery Addresses</Text>
     </HStack>
     <Text align='center'>You decide when <i>these</i> addresses can recover your master key.</Text>
     <Box width='100%'>
       <Input fontSize='xs' width='100%' size='md' mb='0.5em' placeholder='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
         onChange={(event) => {setGuardian(event.target.value);}}
         onKeyDown={(event) => {
-          if (event.key === 'Enter' && isValidAddress) {
-            // add it if its valid
+          if (event.key === 'Enter' && canAddAddress()) {
+            setGuardians((prev) => [prev, guardian].flat(2));
+            setStep(2);
           }
         }}/>
-      { isValidAddress && <Text align='center' fontWeight='bold' textColor='green.600' fontSize='sm'><DisplayAddress address={guardian || ''}/></Text> }
+      { isValidAddress && canAddAddress() &&
+        <Text align='center' fontWeight='bold' textColor='green.600' fontSize='sm'><DisplayAddress address={guardian || ''}/></Text> }
+      { isValidAddress && !canAddAddress() && 
+        <Text align='center' textColor='red.600' fontStyle='italic' fontSize='sm'>Duplicate recovery address</Text> }
       { !isValidAddress && <Text align='center' textColor='red.600' fontStyle='italic' fontSize='sm'>Enter valid address</Text> }
     </Box>
-    <Button width='100%' onClick={()=>{setStep(0);}}>Back</Button>
-    <Button width='100%' isDisabled={!isValidAddress} colorScheme='blue' onClick={()=>{
-      // we can safely blast away the guardian list here as we are
-      // assuming we are choosing the first one
-      setGuardians([guardian]);
+    <Button width='100%' onClick={()=>{setStep(guardians.length > 0 ? 2 : 0);}}>Back</Button>
+    <Button width='100%' isDisabled={!canAddAddress()} colorScheme='blue' onClick={()=>{
+      // it won't be a
+      setGuardians((prev) => [prev, guardian].flat(2));
       setStep(2);
     }}>Next</Button>
   </VStack>
@@ -315,9 +331,9 @@ export function StepTwoContent({keyId, setStep, guardians, setGuardians, ...rest
   };
 
   return <VStack spacing='2em'>
-    <Text fontWeight='bold'>Guardians ({guardians.length})</Text>
-    <List spacing='1em' width='100%'>
-      { guardians.map((g,x) => <ListItem key={'guardian-'+x}>
+    <Text fontWeight='bold'>Recovery Addresses ({guardians.length})</Text>
+    <List spacing='1.5em' width='100%'><AnimatePresence>
+      { guardians.map((g,x) => <motion.div key={'gm'+g} exit={{x: '-100vw'}}><ListItem key={'guardian-'+x}>
         <HStack pos='relative'>
           <AddressAvatar address={g} size={24}/>
           { (address === g) && <Spinner
@@ -334,13 +350,89 @@ export function StepTwoContent({keyId, setStep, guardians, setGuardians, ...rest
           <IconButton size='sm' right='0px' pos='absolute' icon={<FiTrash2 size='22px' color='#ff7b47'/>} borderRadius='full' boxShadow='md'
             onClick={() => {removeGuardian(g);}}/>
         </HStack>
-      </ListItem>) }
+      </ListItem></motion.div>) }
+      </AnimatePresence>
     </List>
-    <Button width='100%'>Add Guardian</Button>
-    <Button width='100%' colorScheme='blue'>Next</Button> 
+    <Button width='100%' onClick={() => {setStep(1);}}>Add Address</Button>
+    <Button width='100%' onClick={() => {setStep(3);}} colorScheme='blue'>Next</Button> 
   </VStack>
 }
 
+export function StepThreeContent({keyId, setStep, guardians, days, setDays, ...rest}) {
+  return <VStack spacing='2em'>
+    <HStack width='100%'>
+      <Tag size='lg'><TagLabel>2</TagLabel></Tag>
+      <Text fontWeight='bold'>Choose Conditions</Text>
+    </HStack>
+    <VStack width='100%'>
+    <HStack width='100%'>
+      <FcAlarmClock size='32px'/>
+      <Text fontWeight='bold'>Inactivity Alarm</Text>
+    </HStack>
+      <Text>Set a timespan to require check-ins with your <b>Master Key</b>.</Text>
+      <Text>Missed check-ins enable one of your <b>recovery addresses</b> to recover a single Master key.</Text>
+    </VStack>
+    <Select variant='filled' size='lg' defaultValue={days} onChange={(e) => {setDays(e.target.value);}}>
+      <option value='30'>Every 30 days</option>
+      <option value='90'>Every 90 days</option>
+      <option value='180'>Every 6 months</option>
+      <option value='365'>Once a year</option>
+      <option value='730'>Once every 2 years</option>
+      <option value='1825'>Once every 5 years</option>
+    </Select>
+    <Button width='100%' colorScheme='blue' onClick={() => {setStep(4);}}>Next</Button>
+  </VStack>
+}
+
+export function StepFourContent({keyId, setStep, guardians, days, ...rest}) {
+  const { address } = useAccount();
+  var date = new Date();
+  date.setDate(date.getDate() + parseInt(days));
+
+  return <VStack spacing='2em'>
+    <Text fontWeight='bold'>Final Review</Text>
+    <VStack width='100%' spacing='1em'>
+    <HStack width='100%'>
+      <Tag size='lg'><TagLabel>1</TagLabel></Tag>
+      <Text fontWeight='bold'>Recovery Addresses</Text>
+      <Spacer/>
+      <Button borderRadius='full' onClick={() => {setStep(2);}}><FiEdit2/></Button>
+    </HStack>
+    <List width='100%' spacing='1.5em' pl='0.25em'>
+      { guardians.map((g,x) => <ListItem key={'grli-'+g}>
+        <HStack pos='relative'>
+          <AddressAvatar address={g} size={24}/>
+          { (address === g) && <Spinner
+              pos='absolute'
+              top='-4px'
+              left='-12px'
+              thickness='2px'
+              speed='2s'
+              color='blue.500'
+              size='lg'
+            /> }
+          <Text fontWeight='bold' fontSize='0.8em'><DisplayAddress address={g}/></Text>
+        </HStack>
+      </ListItem> ) }
+    </List></VStack>
+    <VStack width='100%' spacing='1em'>
+      <HStack width='100%'>
+        <Tag size='lg'><TagLabel>2</TagLabel></Tag>
+        <Text fontWeight='bold'>Conditions</Text>
+        <Spacer/>
+        <Button borderRadius='full' onClick={() => {setStep(3);}}><FiEdit2/></Button>
+      </HStack>
+      <HStack width='100%'>
+        <FcAlarmClock size='32px'/>
+        <Text fontWeight='bold' fontSize='sm'>{days} Day Alarm</Text>
+      </HStack>
+      <Alert status='warning' fontSize='sm'>
+        <AlertIcon/>First check-in: {date.toDateString()}
+      </Alert>
+    </VStack>
+    <Button width='100%' colorScheme='blue'>Enable Recovery Now</Button>
+  </VStack>
+}
 
 export function Recovery() {
   const { keyId } = useParams();
