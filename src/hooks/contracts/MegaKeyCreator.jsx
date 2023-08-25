dfe1f05be4ac736f6b9dfc197d0ef851ed913541
scotthconner <scotthconner@gmail.com>
2023-08-25 16:51:52 -0500

    Set the frontend hook to use the new account interface.

--- src/hooks/contracts/MegaKeyCreator.jsx
+++ src/hooks/contracts/MegaKeyCreator.jsx
@@ -15,7 +15,7 @@ import { Networks } from '../../configuration/Networks';
  * - create a key, send it to the receiver.
  * - create an inbox, copy a key, and soulbind it to the inbox
  */
-export function useMegaKeyCreator(rootKeyId, keyAlias, receiver, bound, errorFunc, successFunc) {
+export function useMegaKeyCreator(rootKeyId, keyAlias, receivers, bound, errorFunc, successFunc) {
   const { address } = useAccount();
   const network = useNetwork();
   const megaKeyAddress = Networks.getContractAddress(network.chain.id, 'MegaKeyCreator');
@@ -26,11 +26,11 @@ export function useMegaKeyCreator(rootKeyId, keyAlias, receiver, bound, errorFun
 
   // encode the data
   var data = ethers.utils.defaultAbiCoder.encode(
-    ['bytes32','address','address','bool'],
-    [ethers.utils.formatBytes32String(keyAlias), provider, receiver, bound]);
+    ['bytes32','address','address[]','bool[]'],
+    [ethers.utils.formatBytes32String(keyAlias), provider, receivers, bound]);
 
    return useLocksmithWrite('KeyVault', 'safeTransferFrom',
       [address, megaKeyAddress, rootKeyId, 1, data],
-      rootKeyId !== null && provider !== null && receiver !== null,
+      rootKeyId !== null && provider !== null && receivers !== null,
       errorFunc, successFunc);
 }
